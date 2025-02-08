280bea138eb41397fdeb56c605827b2c7fe27e7b
EduardOrellana <ore23006@byui.edu>
2025-02-08 14:29:43 -0600

    fixing errors

--- routes/prophets.js
+++ routes/prophets.js
@@ -4,48 +4,48 @@ const router = express.Router();
 const prophetsController = require('../controllers/prophets');
 
 function handleError(res, error) {
-  console.error(error); 
-  res.status(500).json({ message: "Internal Server Error", error: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", error: error.message });
 }
 
 router.get('/', async (req, res) => {
     try {
-      await prophetsController.getAll(req, res);
+        await prophetsController.getAll(req, res);
     } catch (error) {
-      handleError(res, error);
+        handleError(res, error);
     }
-  });
-  
-  router.get('/:id', async (req, res) => {
+});
+
+router.get('/:id', async (req, res) => {
     try {
-      await prophetsController.getSingle(req, res);
+        await prophetsController.getSingle(req, res);
     } catch (error) {
-      handleError(res, error);
+        handleError(res, error);
     }
-  });
-  
-  router.post('/',  async (req, res) => {
+});
+
+router.post('/', async (req, res) => {
     try {
-      await prophetsController.createProphets(req, res);
+        await prophetsController.createProphets(req, res);
     } catch (error) {
-      handleError(res, error);
+        handleError(res, error);
     }
-  });
-  
-  router.put('/:id', async (req, res) => {
+});
+
+router.put('/:id', async (req, res) => {
     try {
-      await prophetsController.updateProphets(req, res);
+        await prophetsController.updateProphets(req, res);
     } catch (error) {
-      handleError(res, error);
+        handleError(res, error);
     }
-  });
-  
-  router.delete('/:id', async (req, res) => {
+});
+
+router.delete('/:id', async (req, res) => {
     try {
-      await prophetsController.deleteProphets(req, res);
+        await prophetsController.deleteProphets(req, res);
     } catch (error) {
-      handleError(res, error);
+        handleError(res, error);
     }
-  });
+});
 
 module.exports = router;
